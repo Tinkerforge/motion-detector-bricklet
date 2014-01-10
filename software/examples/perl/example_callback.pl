6893f74884d9af767e4086db4743bb62b5e01bea
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-01-10 15:58:31 +0100

    Add Perl examples

---
+++ software/examples/perl/example_callback.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletMotionDetector;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'jXm'; # Change to your UID
+
+# Callback function for end of detection cycle
+sub cb_detection_cycle_ended
+{
+    print "\nDetection Cycle Ended (next detection possible in ~3 seconds)\n";
+}
+
+# Callback function for detected motion
+sub cb_motion_detected
+{
+    print "\nMotion Detected\n";
+}
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $md = BrickletMotionDetector->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Register detected callback to function cb_motion_detected
+$md->register_callback($md->CALLBACK_MOTION_DETECTED, 'cb_motion_detected');
+
+# Register detection cycle ended callback to function cb_detection_cycle_ended
+$md->register_callback($md->CALLBACK_DETECTION_CYCLE_ENDED, 'cb_detection_cycle_ended');
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
+
