13f0a7b9148303848e760d6fdedef0cb45a3688a
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-01-10 15:59:12 +0100

    Comment fixed: cb_detection_cycle_ended

--- software/examples/python/example_callback.py
+++ software/examples/python/example_callback.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     # Register detected callback to function cb_motion_detected
     md.register_callback(md.CALLBACK_MOTION_DETECTED, cb_motion_detected)
 
-    # Register detection cycle ended callback to function DetectionCycleEndedCB
+    # Register detection cycle ended callback to function cb_detection_cycle_ended
     md.register_callback(md.CALLBACK_DETECTION_CYCLE_ENDED, cb_detection_cycle_ended)
 
     raw_input('Press key to exit\n') # Use input() in Python 3
