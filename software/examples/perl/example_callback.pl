dd3a329152b54ae9174a873e05b87fcb3c80f79c
Matthias Bolte <matthias@tinkerforge.com>
2014-04-02 19:30:37 +0200

    perl: Unify print style

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -10,13 +10,13 @@ use constant UID => 'jXm'; # Change to your UID
 # Callback function for end of detection cycle
 sub cb_detection_cycle_ended
 {
-    print "\nDetection Cycle Ended (next detection possible in ~3 seconds)\n";
+    print "Detection Cycle Ended (next detection possible in ~3 seconds)\n";
 }
 
 # Callback function for detected motion
 sub cb_motion_detected
 {
-    print "\nMotion Detected\n";
+    print "Motion Detected\n";
 }
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
@@ -31,7 +31,7 @@ $md->register_callback($md->CALLBACK_MOTION_DETECTED, 'cb_motion_detected');
 # Register detection cycle ended callback to function cb_detection_cycle_ended
 $md->register_callback($md->CALLBACK_DETECTION_CYCLE_ENDED, 'cb_detection_cycle_ended');
 
-print "\nPress any key to exit...\n";
+print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
 
