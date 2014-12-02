13a2a893a95992887b5a123d460dd351784afbf4
Olaf Lüke <olaf@tinkerforge.com>
2014-12-02 16:54:57 +0100

    Use ReadLine instead of ReadKey for C# example

--- software/examples/csharp/ExampleCallback.cs
+++ software/examples/csharp/ExampleCallback.cs
@@ -32,8 +32,8 @@ class Example
 		// Register detection cycle ended callback to function DetectionCycleEndedCB
 		motion_detector.DetectionCycleEnded += DetectionCycleEndedCB;
 
-		System.Console.WriteLine("Press key to exit");
-		System.Console.ReadKey();
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
 		ipcon.Disconnect();
 	}
 }
