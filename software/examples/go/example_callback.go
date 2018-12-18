6f0958459e34920ecdd1993ce7b2f415eb592cba
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:21:40 +0100

    go: Change import path again.

--- software/examples/go/example_callback.go
+++ software/examples/go/example_callback.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/tinkerforge/go-api-bindings/ipconnection"
-	"github.com/tinkerforge/go-api-bindings/motion_detector_bricklet"
+	"github.com/Tinkerforge/go-api-bindings/ipconnection"
+	"github.com/Tinkerforge/go-api-bindings/motion_detector_bricklet"
 )
 
 const ADDR string = "localhost:4223"
